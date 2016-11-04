a0e3344d37e5577f860811bd3131dffa688f51c8
dario <darioyzf@gmail.com>
2016-11-04 15:25:05 +0100

    oki

--- index.ts
+++ index.ts
@@ -8,11 +8,12 @@ const exec = child_process.exec
 export default function (pkg: string) {
     return new Promise((resolve, reject) => {
         exec(__dirname + '/script.sh ' + pkg, (err, out, stderr) => {
-            if (!err) {
-                if (out && (out === 'true' || out === 'false')) {
-                    resolve(eval(out))
+            if (!err && out) {
+                const o = out.toString()
+                if (o === 'true' || o === 'false') {
+                    resolve(eval(o))
                 } else {
-                    reject("error! " + out)
+                    reject("error! " + o)
                 }
             } else {
                 reject(err)
